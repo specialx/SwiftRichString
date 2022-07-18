72ca04d4d0232ef16ae74f2c7b38ee7079b425a9
ascj <ascj@me.com>
2022-07-18 13:25:10 -0400

    missing comma

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [
         .macOS(.v10_11),
         .iOS(.v8)
-    ]
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
