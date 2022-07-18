8de5bebdddc59f1a2d1b3beb4d046b77772f12d5
ascj <ascj@me.com>
2022-07-18 13:24:52 -0400

    minimum versions

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftRichString",
+    platforms: [
+        .macOS(.v10_11),
+        .iOS(.v8)
+    ]
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
