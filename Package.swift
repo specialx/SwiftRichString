62d9590297e9ceb20308eafed48bb9b13361314d
ascj <ascj@me.com>
2022-07-19 11:01:19 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SwiftRichString",
     platforms: [
         .macOS(.v10_11),
-        .iOS(.v8)
+        .iOS(.v9)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
